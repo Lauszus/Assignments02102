452e5de4b5002d61adef35daa36e9b25a4dfe164
Kristian Sloth Lauszus <lauszus@gmail.com>
2013-04-09 20:10:39 +0200

    Added GameOfLife files

---
+++ src/afleveringsopgave4/opgave3/Lauszus/GameOfLife.java
@@ -0,0 +1,9 @@
+package afleveringsopgave4.opgave3.Lauszus;
+
+public class GameOfLife {
+
+	public GameOfLife() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
---
+++ src/afleveringsopgave4/opgave3/Lauszus/GameOfLifeMain.java
@@ -0,0 +1,13 @@
+package afleveringsopgave4.opgave3.Lauszus;
+
+public class GameOfLifeMain {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
