5f18e770d9ee19ca8d3d34398e52003f4090c148
Kristian Sloth Lauszus <lauszus@gmail.com>
2013-04-09 22:53:01 +0200

    GOL working!

--- src/afleveringsopgave4/opgave3/Lauszus/GameOfLife.java
+++ src/afleveringsopgave4/opgave3/Lauszus/GameOfLife.java
@@ -1,9 +1,72 @@
 package afleveringsopgave4.opgave3.Lauszus;
 
+import java.util.Random;
+
 public class GameOfLife {
-
-	public GameOfLife() {
-		// TODO Auto-generated constructor stub
+	private static Random random = new Random();
+	private static int[][] state;	
+	private static int gridSize;
+	
+	
+	public GameOfLife(int n) {
+		gridSize = n;
+		state = new int[n][n];
+		for(int y = 0; y < gridSize; y++) {
+			for(int x = 0; x < gridSize; x++)
+				state[y][x] = random.nextInt(2); // Returns a value from 0-1
+		}
 	}
-
+	
+	public GameOfLife(int[][] initialState) {
+		gridSize = initialState[0].length; // We will assume that it's symmetrical 
+		state = initialState.clone();
+	}
+	
+	public void nextState() {
+		int[][] newState = new int[gridSize][gridSize];
+		for(int y = 0; y < gridSize; y++) {
+			for(int x = 0; x < gridSize; x++) {
+				int liveNeighbours = liveNeighbours(x,y);
+				if(liveNeighbours < 2 || liveNeighbours > 3)
+					newState[y][x] = 0;
+				else if(liveNeighbours == 3)
+					newState[y][x] = 1;
+				else
+					newState[y][x] = getState(x, y);
+			}
+		}
+		state = newState.clone();
+	}
+	
+	private int getState(int x, int y) {
+		return state[y][x];		
+	}
+	
+	public int liveNeighbours(int x, int y) {
+		int liveNeighbours = 0;
+		
+		for(int i = -1; i <= 1; i++) {
+			int corY = y+i;
+			for(int j = -1; j <= 1; j++) {
+				int corX = x+j;
+				
+				if(corX >= 0 && corY >= 0 && corX < gridSize && corY < gridSize && (corX != x || corY != y)) {
+					if(getState(corX,corY) == 1)
+						liveNeighbours++;
+				}
+			}
+		}
+		return liveNeighbours;
+	}
+	
+	public String toString() {
+		String output = "";
+		for(int y = 0; y < gridSize; y++) {
+			for(int x = 0; x < gridSize; x++) {
+				output += getState(x,y) + " ";
+			}
+			output += "\n";
+		}
+		return output;
+	}
 }
--- src/afleveringsopgave4/opgave3/Lauszus/GameOfLifeMain.java
+++ src/afleveringsopgave4/opgave3/Lauszus/GameOfLifeMain.java
@@ -1,13 +1,25 @@
 package afleveringsopgave4.opgave3.Lauszus;
 
 public class GameOfLifeMain {
+	private static int[][] initialState = {
+		{1,0,1},
+		{0,1,1},
+		{0,1,0},
+	};
 
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		GameOfLife gol = new GameOfLife(initialState);		
+		for(int i = 0; i < 6; i++) {
+			System.out.print("\n" + gol);
+			gol.nextState();
+		}
+		/*
+		GameOfLife gol = new GameOfLife(3);
+		System.out.print(gol);
+		System.out.println(gol.liveNeighbours(0,1));
+		*/
 	}
-
 }
