94cbeea3e91f083328e0021357636e007a29ab1f
Kristian Sloth Lauszus <lauszus@gmail.com>
2013-04-10 00:58:46 +0200

    Can now load initial state from file
    Also added documentation

--- src/afleveringsopgave4/opgave3/Lauszus/GameOfLife.java
+++ src/afleveringsopgave4/opgave3/Lauszus/GameOfLife.java
@@ -2,12 +2,19 @@ package afleveringsopgave4.opgave3.Lauszus;
 
 import java.util.Random;
 
+/**
+ * This is the Game of Life class which will keep track of the current state and generate the next state.
+ * @author Simon Patrzalek, Mads Bornebusch and Kristian Lauszus.
+ */
 public class GameOfLife {
 	private static Random random = new Random();
 	private static int[][] state;	
-	private static int gridSize;
+	private static int gridSize;	
 	
-	
+	/**
+	 * Constructor used to set a random state.
+	 * @param n Size of the n x n array.
+	 */
 	public GameOfLife(int n) {
 		gridSize = n;
 		state = new int[n][n];
@@ -17,11 +24,18 @@ public class GameOfLife {
 		}
 	}
 	
+	/**
+	 * Constructor used to set the initial state.
+	 * @param initialState A n x n array containing the initial state.   
+	 */
 	public GameOfLife(int[][] initialState) {
 		gridSize = initialState[0].length; // We will assume that it's symmetrical 
 		state = initialState.clone();
 	}
 	
+	/**
+	 * Call this to get the next state in the game.
+	 */
 	public void nextState() {
 		int[][] newState = new int[gridSize][gridSize];
 		for(int y = 0; y < gridSize; y++) {
@@ -38,11 +52,23 @@ public class GameOfLife {
 		state = newState.clone();
 	}
 	
-	public int getState(int x, int y) { // Return the state with y-coordinates starting at the bottom 
+	/**
+	 * Return the state at the specific x,y-coordinate.
+	 * @param x x-coordinate.
+	 * @param y y-coordinate.
+	 * @return The state with y-coordinates starting at the bottom for easy use with StdDraw.
+	 */
+	public int getState(int x, int y) {
 		y = gridSize-1 - y;	
 		return state[y][x];		
 	}
 	
+	/**
+	 * Function used to count how many neighbours a point has.
+	 * @param x-coordinate of the point.
+	 * @param y-coordinate of the point.
+	 * @return Returns the numbers of neighbours.
+	 */
 	public int liveNeighbours(int x, int y) {
 		int liveNeighbours = 0;
 		
@@ -50,7 +76,6 @@ public class GameOfLife {
 			int corY = y+i;
 			for(int j = -1; j <= 1; j++) {
 				int corX = x+j;
-				
 				if(corX >= 0 && corY >= 0 && corX < gridSize && corY < gridSize && (corX != x || corY != y)) {
 					if(state[corY][corX] == 1)
 						liveNeighbours++;
@@ -60,6 +85,9 @@ public class GameOfLife {
 		return liveNeighbours;
 	}
 	
+	/**
+	 * Converts the current state into a printable matrix.
+	 */
 	public String toString() {
 		String output = "";
 		for(int y = 0; y < gridSize; y++) {
