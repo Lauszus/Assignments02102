d63f5a0513c03bab5398b35a5b79519c67bf1e31
Kristian Sloth Lauszus <lauszus@gmail.com>
2013-04-14 21:58:48 +0200

    Implemented FileChooser

--- src/afleveringsopgave4/opgave3/Lauszus/GameOfLifeMain.java
+++ src/afleveringsopgave4/opgave3/Lauszus/GameOfLifeMain.java
@@ -7,6 +7,8 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
+import javax.swing.JFileChooser;
+
 /**
  * This is the main game of life class. This will create an instance of GameOfLife and draw in on the screen.
  * @author Simon Patrzalek, Mads Bornebusch and Kristian Lauszus.
@@ -27,22 +29,27 @@ public class GameOfLifeMain {
 	 * @throws FileNotFoundException Called when the requested initial file doesn't exist.
 	 */
 	public static void main(String[] args) throws FileNotFoundException {
+		JFileChooser chooser = new JFileChooser(new File(System.getProperty("user.dir")));
+	    int returnVal = chooser.showOpenDialog(null);
+	    if(returnVal != JFileChooser.APPROVE_OPTION)
+	    	return;
+	    
+		GameOfLife gol = new GameOfLife(openMatrix(chooser.getSelectedFile().getPath())); // Read the initial state from a file
+		System.out.println(gol); // Print the state as a matrix
+		
 		/*GameOfLife gol = new GameOfLife(initialState); // Use the array above to set the initial state
 		gridSize = initialState[0].length;*/
 		//GameOfLife gol = new GameOfLife(gridSize); // Create random game
-		GameOfLife gol = new GameOfLife(openMatrix("glider_gun.gol")); // Read the initial state from a file
-		System.out.println(gol); // Print the state as a matrix
 		
 		StdDraw.setCanvasSize(700, 700); // Adjust the size of the window
 		StdDraw.setBorder(0.10); // We added this function ourself in order to adjust the border size
 		StdDraw.setScale(0, gridSize-1); // Set size of the coordinate system
 		StdDraw.setPenRadius(1.0/(double)gridSize);
-		StdDraw.setPenColor(Color.BLACK);		
+		StdDraw.setPenColor(Color.BLACK);
 		
 		for(int i = 1; i <= 500; i++) {
 			StdDraw.show(0);
 			StdDraw.clear();
-			//StdDraw.textLeft(-1.5/(double)gridSize, gridSize+10.0/(double)gridSize, Integer.toString(i));
 			
 			for(int y = 0; y < gridSize; y++) {
 				for(int x = 0; x < gridSize; x++) {
@@ -50,20 +57,20 @@ public class GameOfLifeMain {
 						StdDraw.point(x, y);
 				}
 			}
-			StdDraw.show(25); // Sleep for 25ms
+			StdDraw.show(50); // Sleep for 50ms
 			gol.nextState();
 		}
 	}
 	
 	/**
 	 * Used to open a file containing a n x n dimensional array with an initial state.
-	 * @param filename The name of the file you want to open located in the "src/afleveringsopgave4/Files/gol" directory.
+	 * @param filepath The name of the file you want to open located in the "src/afleveringsopgave4/Files/gol" directory.
 	 * @return Return a n x n dimensional array with all the values in the matrix.
 	 * @throws FileNotFoundException Throws this error in case the file doesn't exist.
 	 */
-	private static int[][] openMatrix(String filename) throws FileNotFoundException {
+	private static int[][] openMatrix(String filepath) throws FileNotFoundException {
 		List<Integer> list = new ArrayList<Integer>();
-		Scanner fileScanner = new Scanner(new File("src" + File.separator + "afleveringsopgave4" + File.separator + "Files" + File.separator + "gol" + File.separator + filename));
+		Scanner fileScanner = new Scanner(new File(filepath));
 		
 		while(fileScanner.hasNextInt())
 			list.add(fileScanner.nextInt());
