09b5ca2cad005a61dd71a6b7e39d5b3701cdfdd3
Kristian Sloth Lauszus <lauszus@gmail.com>
2013-05-10 22:54:13 +0200

    Check if there is actually something in the array

--- src/afleveringsopgave5/Opgaver Kristian/Opg2/stack.c
+++ src/afleveringsopgave5/Opgaver Kristian/Opg2/stack.c
@@ -3,11 +3,14 @@
 #include "stack.h"
 
 int pop(my_stack * stack_p) {
-	stack_p->size--;
-	return stack_p->array[stack_p->size];
+	if (stack_p->size > 0) { // Check if there is actually something in the array
+		stack_p->size--;
+		return stack_p->array[stack_p->size];
+	}
+	return -1;
 }
 void push(my_stack * stack_p, int value) {
-	if(stack_p->size >= stack_p->capacity) { // It's already full
+	if (stack_p->size >= stack_p->capacity) { // It's already full
 		stack_p->capacity = stack_p->capacity*2;
 		stack_p->array = (int*)realloc(stack_p->array, stack_p->capacity*sizeof(int));
 	}	
@@ -15,7 +18,9 @@ void push(my_stack * stack_p, int value) {
 	stack_p->size++;
 }
 int top(my_stack * stack_p) {
-	return stack_p->array[stack_p->size-1];
+	if (stack_p->size > 0) // Check if there is actually something in the array
+		return stack_p->array[stack_p->size-1];
+	return -1;
 }
 my_stack * newStack(void) {
 	my_stack * stack_p = (my_stack*)malloc(sizeof(my_stack));
